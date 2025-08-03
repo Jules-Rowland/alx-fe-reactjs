e4ba2417a8a671302b492d68f2e5d169a16fe4cd
Jules.Rowland <Jules.rowland02@gmail.com>
2025-08-03 04:41:56 +0100

    Working with Context API

---
+++ alx-react-app-props/src/components/DoubleBox.jsx
@@ -0,0 +1,19 @@
+function DoubleBox() {
+  return (
+    <div style ={{
+        border: '2px solid blue',
+        margin: '10px',
+        padding: '20px',
+        textAlign: 'center',}}>
+        <h1>Welcome</h1>
+
+    <div style ={{
+       border: '2px solid blue',
+        margin: '10px',
+        padding: '20px',
+        textAlign: 'center',
+    }}>Click Me</div>
+    </div>
+  );
+}
+export default  DoubleBox;

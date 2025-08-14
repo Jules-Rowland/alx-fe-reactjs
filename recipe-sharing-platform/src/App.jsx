b2fbed3cfe78fa1954d8676bd7fb9a6fdbd16cc9
Jules.Rowland <Jules.rowland02@gmail.com>
2025-08-14 15:50:26 +0100

    Setting up a new Project with Tailwind framework

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-blue-500'>Welcome to my Page</div>
+    </>
+  )
+}
+
+export default App

fbc236110ba3a0698e0ac4abcb87fa2147e8cedf
Jules.Rowland <Jules.rowland02@gmail.com>
2025-08-16 21:58:23 +0100

    Adjustment made to Card style

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -11,7 +11,7 @@ function Homepage () {
    return(
     <div className ="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 " > 
     {recipe.map(item =>(
-            <div className='flex flex-col   hover:shadow-xl hover:scale-110 
+            <div className='flex flex-col   hover:shadow-xl hover:scale-110 rounded
             transition-shadow duration-600 hover:shadow-black-500/50 ' key = {item.id}>
                 <div className='h-48 overflow-hidden'><img className='w-36 h-36 sm: w-40 sm: h-40 md: w-44 md: h-44 lg: w-48 lg: h-48 
                 object-cover' src={item.image} alt="Recipe-Image" /> </div>
